d5f2232cda79cdbaa191b5706c7046c689776d9b
jaijaish98 <jaijaish98@gmail.com>
2020-05-14 08:21:43 +0530

    Remove K digits to make a number smaller

---
+++ remove_k_digits_to_make_a_number_smaller.py
@@ -0,0 +1,45 @@
+"""
+Remove K Digits
+
+Given a non-negative integer num represented as a string, remove k digits from the number so that the new number is the smallest possible.
+
+Note:
+The length of num is less than 10002 and will be ≥ k.
+The given num does not contain any leading zero.
+Example 1:
+
+Input: num = "1432219", k = 3
+Output: "1219"
+Explanation: Remove the three digits 4, 3, and 2 to form the new number 1219 which is the smallest.
+Example 2:
+
+Input: num = "10200", k = 1
+Output: "200"
+Explanation: Remove the leading 1 and the number is 200. Note that the output must not contain leading zeroes.
+Example 3:
+
+Input: num = "10", k = 2
+Output: "0"
+Explanation: Remove all the digits from the number and it is left with nothing which is 0.
+
+"""
+
+
+class Solution:
+    @staticmethod
+    def remove_k_digits(num: str, k: int) -> str:
+        stack = []
+        for digit in num:
+            while k > 0 and len(stack) > 0 and stack[-1] > digit:
+                k -= 1
+                stack.pop()
+            stack.append(digit)
+        if k > 0:
+            stack = stack[:-k]
+        return "".join(stack).lstrip("0") or "0"
+
+
+if __name__ == "__main__":
+    res = Solution()
+    result = res.remove_k_digits("10200", 1)
+    print(result)
