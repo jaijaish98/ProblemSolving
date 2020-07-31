bdaf8e2e45bd9206e9798c3e5bbec2eafb2133f7
jaijaish98 <jaijaish98@gmail.com>
2020-07-31 17:07:08 +0530

    Intersection of two arrays with repeated numbers

--- intersection_of_two_arrays.py
+++ intersection_of_two_arrays.py
@@ -25,10 +25,37 @@ class Solution:
         set2 = set(nums2)
         return list(set1 & set2)
 
+    def intersect(self, nums1, nums2):
+
+        def find_intersect(nums1, nums2):
+
+            result = []
+            hashMap = {}
+
+            for i in range(len(nums1)):
+                if hashMap.get(nums1[i]):
+                    hashMap[nums1[i]] += 1
+                else:
+                    hashMap[nums1[i]] = 1
+
+            for i in range(len(nums2)):
+                if hashMap.get(nums2[i]):
+                    if hashMap[nums2[i]] > 0:
+                        hashMap[nums2[i]] -= 1
+                        result.append(nums2[i])
+
+            return result
+
+        if len(nums1) > len(nums2):
+            return find_intersect(nums2, nums1)
+        else:
+            return find_intersect(nums1, nums2)
+
 
 if __name__ == "__main__":
     inter = Solution()
-    nums1 = [4, 9, 5]
+    nums1 = [4, 9, 5, 9]
     nums2 = [9, 4, 9, 8, 4]
     result_arr = inter.intersection(nums1, nums2)
     print(result_arr)
+    print(inter.intersect(nums1, nums2))
